4c9bd185e82cb8f64988815b2333ec0d38b6c8ca
Harshitha799 <80540379+Harshitha799@users.noreply.github.com>
2021-04-22 10:55:06 +0530

    Update IFeedBackRepository.java

--- Online Bus Booking/OnlineBusBooking-App-Pro/bin/com/cg/repository/IFeedBackRepository.java
+++ Online Bus Booking/OnlineBusBooking-App-Pro/bin/com/cg/repository/IFeedBackRepository.java
@@ -2,6 +2,9 @@ package com.cg.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.cg.entities.BusOperator;
 import com.cg.entities.FeedBack;
 
