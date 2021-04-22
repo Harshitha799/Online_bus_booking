1f1fb58ca9e463b832c8128e4985e8d94930327c
Harshitha799 <80540379+Harshitha799@users.noreply.github.com>
2021-04-22 11:09:55 +0530

    Update IUsersService.java

--- Online Bus Booking/OnlineBusBooking-App-Pro/bin/com/cg/service/IUsersService.java
+++ Online Bus Booking/OnlineBusBooking-App-Pro/bin/com/cg/service/IUsersService.java
@@ -2,16 +2,18 @@ package com.cg.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.cg.entities.Booking;
 import com.cg.entities.User;
 import com.cg.exceptions.InvalidBookingIdException;
 
+@Service
 public interface IUsersService {
 
 	public void addUser(User user);
 	public void deleteUser(String username);
 	public void updatePassword(String username,String newPassword);
-	
 	
 	public User singIn(User user);
 	public User singOut(User user);
